692745ab207514a8d73f731b65f11095b0c9109c
Andy <arn.ramirezr@gmail.com>
2021-07-04 11:47:37 -0400

    Trabajo finalizado

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();
+
+    $("#enviarCorreo").click(function(){
+    alert("El correo fue enviado correctamente....");
+  });
+
+});
+
+
+
